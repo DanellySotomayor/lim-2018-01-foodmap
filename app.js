de15fe9c7c549559316561009dedd4c831b5a8ca
Danelly <danelly.lucia@gmail.com>
2018-08-02 12:31:24 -0500

    función mapinit y buscador responsive

--- app.js
+++ app.js
@@ -1,3 +1,44 @@
-window.setTimeout(function() {
-    window.location.href = '../views/home.html';
-}, 2000);
+var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 15
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Estás aquí');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
+    
+    //   function initMap(posicion) {
+    //     let latitude = posicion.coords.latitude;
+    //     let longitude = posicion.coords.longitude;
+    //     let imgUrl = "https://maps.googleapis.com/maps/api/staticmap?center="+latitude+","+longitude+"&size=600x300&markers=colorred%7C"+latitude+","+longitude+"&key=AIzaSyCUO75R0V07gBfxjOSmCav9UBIb-MUD5yg";
+
+    //     output.innerHTML = "<img src='"+imgUrl+"'>";
+    //   }
